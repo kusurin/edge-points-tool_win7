b708371b6c1550cba6dd2ba0649b671451d9494f
风声疏狂 <1328379657@qq.com>
2023-07-23 07:43:55 +0800

    取消setCookies.py的无窗口运行

--- setCookies.py
+++ setCookies.py
@@ -4,9 +4,7 @@ import time
 import json
 
 # 填写webdriver的保存目录
-options = Options()
-options.add_argument("--headless")  # 设置后台运行，无窗口化
-driver = webdriver.Edge('C:/Users/Windy/EdgeDriver/msedgedriver', options=options)
+driver = webdriver.Edge('C:/Users/Windy/EdgeDriver/msedgedriver')
 
 # 记得写完整的url 包括http和https
 driver.get('https://cn.bing.com/')
